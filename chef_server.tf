8d6ce8023d687ab8a70b08ca1d9142285e235ec5
valenbb <valenbb@gmail.com>
2018-09-08 09:29:20 -0400

    Chagned userdata to use file

--- chef_server.tf
+++ chef_server.tf
@@ -52,23 +52,7 @@ resource "aws_instance" "chef_server" {
   availability_zone 		= "us-east-1b"
   subnet_id 			= "subnet-07f4345b" 
   associate_public_ip_address 	= true
-  user_data			= <<-EOF
-				  #!/bin/bash
-                                  apt update -y
-				  wget https://packages.chef.io/files/current/chef-server/12.18.13/ubuntu/18.04/chef-server-core_12.18.13-1_amd64.deb -P /tmp
-				  dpkg -i /tmp/chef-server-core_12.18.13-1_amd64.deb
-				  chef-server-ctl reconfigure
-                                  mkdir -p .chef
-				  chef-server-ctl user-create admin Admin User andy.gitc@gmail.com 'Che3f@AWS' --filename /root/.chef/chef.pem
-				  chef-server-ctl org-create esn ‘MyXingfu Net’ –association_user admin --filename /root/.chef/esn-validator.pem
-				  chef-server-ctl install chef-manage
-				  chef-server-ctl reconfigure
-				  chef-manage-ctl reconfigure –accept-license
-				  chef-server-ctl install opscode-reporting
-				  chef-server-ctl reconfigure
-				  opscode-reporting-ctl reconfigure –accept-license
-				  apt upgrade -y                                 
-				  EOF
+  user_data			= "${file("chef_server.sh")}"
  
   tags {
     Name 	= "CHEF-SERVER-PRD"
