2291b591e92e32641c1dac0f78344b93828edb98
valenbb <valenbb@gmail.com>
2018-09-08 22:41:07 -0400

    Using variables and output tf files

--- chef_server.tf
+++ chef_server.tf
@@ -1,32 +1,32 @@
 provider "aws" {
   version = "~> 1.35"
-  region = "us-east-1"
+  region = "${var.aws_region}"
 }
 
 resource "aws_security_group" "chef_sg" {
-  name 		= "CHEF-SERVER-SG"
+  name 		= "{var.tag_name}-SG"
   description 	= "Allow inbound traffic from home only"
-  vpc_id      	= "vpc-ce4b4eb5"
+  vpc_id      	= "${var.vpc_prod_id}"
 
   ingress {
     from_port	= 22
     to_port	= 22
     protocol	= "tcp"
-    cidr_blocks	= ["100.4.210.205/32"]
+    cidr_blocks	= ["{var.cidr_access}"]
   }
   
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["100.4.210.205/32"]
+    cidr_blocks = ["{var.cidr_access}"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["100.4.210.205/32"]
+    cidr_blocks = ["{var.cidr_access}"]
   }
   
   egress {
@@ -37,28 +37,28 @@ resource "aws_security_group" "chef_sg" {
   }
 
   tags {
-    Name 	= "CHEF-SERVER-SG"
-    owner	= "Andy Lalla"
-    env		= "prd"
-    builder	= "terraform"
+    Name 	= "{var.tag_name}-SG"
+    owner	= "{var.tag_owner}"
+    env		= "{var.tag_env}"
+    builder	= "{var.tag_builder}"
   }
 }
 
 resource "aws_instance" "chef_server" {
-  ami           		= "ami-04169656fea786776"
-  instance_type 		= "t2.medium"
+  ami           		= "${var.aws_ami}"
+  instance_type 		= "${var.instance_type}"
   vpc_security_group_ids 	= ["${aws_security_group.chef_sg.id}"]
-  key_name			= "terraform"  
-  availability_zone 		= "us-east-1b"
-  subnet_id 			= "subnet-07f4345b" 
+  key_name			= "${var.key_name}"  
+  availability_zone 		= "${var.az_id}"
+  subnet_id 			= "${var.subnet_id}" 
   associate_public_ip_address 	= true
   user_data			= "${file("chef_server.sh")}"
  
   tags {
-    Name 	= "CHEF-SERVER-PRD"
-    owner	= "Andy Lalla"
-    env		= "prd"
-    builder	= "terraform" 
+    Name 	= "{var.tag_name}-{var.tag_env}"
+    owner	= "{var.tag_owner}"
+    env		= "{var.tag_env}"
+    builder	= "{var.tag_builder}"
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,61 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Desired instance type for EC2"
+  default = "t2.medium"
+}
+
+# CentOS 7 AMI
+variable "aws_ami" {
+  default = "ami-0eaf7f3ca4d177abb"
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+  default = "terraform"
+}
+
+variable "vpc_prod_id" {
+  description = "Production VPC ID"
+  default = "vpc-ce4b4eb5"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to use"
+  default = "subnet-07f4345b"
+}
+
+variable "cidr_access" {
+  description = "CIDR of originating traffic"
+  default = "100.4.210.205/32"
+}
+
+variable "az_id" {
+  description = "Availability Zone"
+  default = "us-east-1b"
+}
+
+variable "tag_name" {
+  description = "Name tag value"
+  default = "CHEF-SERVER"
+}
+
+
+variable "tag_owner" {
+  description = "Owner tag value"
+  default = "Andy Lalla"
+}
+
+variable "tag_env" {
+  description = "Environment tag value"
+  default = "PROD"
+}
+
+
+variable "tag_builder" {
+  description = "Provisioning tool tag value"
+  default = "Terraform"
+}
