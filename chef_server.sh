671f5e5380d484751a374d0e82eea7c16a84dd04
valenbb <valenbb@gmail.com>
2018-09-08 22:44:24 -0400

    Changed install script for CentOS 7

--- chef_server.sh
+++ chef_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt update -y
-wget https://packages.chef.io/files/current/chef-server/12.18.13/ubuntu/18.04/chef-server-core_12.18.13-1_amd64.deb -P /tmp
-dpkg -i /tmp/chef-server-core_12.18.13-1_amd64.deb
+yum update -y
+wget https://packages.chef.io/files/stable/chef-server/12.17.33/el/7/chef-server-core-12.17.33-1.el7.x86_64.rpm -P /tmp
+rpm -ivh /tmp/chef-server-core_12.18.13-1_amd64.deb
 chef-server-ctl reconfigure
 mkdir -p .chef
 chef-server-ctl user-create admin Admin User andy.gitc@gmail.com 'Che3f@AWS' --filename /root/.chef/chef.pem
@@ -13,5 +13,4 @@ chef-manage-ctl reconfigure –accept-license
 chef-server-ctl install opscode-reporting
 chef-server-ctl reconfigure
 opscode-reporting-ctl reconfigure –accept-license
-apt upgrade -y
 
